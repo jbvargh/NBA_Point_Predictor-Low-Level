2d1dda0d0c8a24fd33e495ea0337caa2618158e3
jbvargh <joshvarghese47@gmail.com>
2026-01-08 13:10:47 -0500

    finished very basic predictor

--- check_player_trend.py
+++ check_player_trend.py
@@ -2,15 +2,21 @@ import pandas as pd
 from nba_api.stats.static import players
 from nba_api.stats.endpoints import playergamelog
 
-def check_player_trend(player_name):
+def check_player_trend(player_name, is_home):
     p = players.find_players_by_full_name(player_name)[0]
     id = p['id']
     log = playergamelog.PlayerGameLog(player_id = id, season='2025-26')
     df = log.get_data_frames()[0]
+    print(df.head())
     if df.empty:
         return f"Error: {player_name} has not played any games in 2025-26."
-    season_avg = df['PTS'].mean()
     last_avg = df.head(10)['PTS'].mean()
+    if is_home:
+        season_avg = df[df['MATCHUP'].str.contains('vs.')]['PTS'].mean()
+    else:
+        season_avg = df[df['MATCHUP'].str.contains('@')]['PTS'].mean()
+    if pd.isna(season_avg):
+        season_avg = df['PTS'].mean()
     trend = "Neutral"
     if last_avg - season_avg >= 3.0:
         trend = "Heating Up"
--- game_predictor.py
+++ game_predictor.py
@@ -12,12 +12,12 @@ def calculate_ewma(all_season_scores, span):
         ewma = alpha * score + (1 - alpha) * ewma
     return ewma
 
-def predict_points(player_name, opposing_team):
-    player_info = check_player_trend(player_name)
+def predict_points(player_name, opposing_team, is_home):
+    player_info = check_player_trend(player_name, is_home)
     stats = leaguedashteamstats.LeagueDashTeamStats(season='2025-26', per_mode_detailed='PerGame', measure_type_detailed_defense='Base')
     df = stats.get_data_frames()[0]
     df['OPP_PTS'] = df['PTS'] - df['PLUS_MINUS']
-    opp_pts = df.loc[df['TEAM_NAME'] == opposing_team, 'OPP_PTS'].iloc[0]
+    opp_pts = df.loc[df['TEAM_NAME'].str.contains(opposing_team, case=False), 'OPP_PTS'].iloc[0]
     factor = opp_pts / df['OPP_PTS'].mean()
     baseline = 0.6 * calculate_ewma(player_info['all_point_totals'], 10) + 0.4 * player_info["season_ppg"]
     #print("factor: ", factor)
@@ -26,4 +26,4 @@ def predict_points(player_name, opposing_team):
 
 
 if __name__=="__main__":
-    print(round(predict_points("Luka Doncic", "New Orleans Pelicans"), 1))
+    print(round(predict_points("Giannis Antetokounmpo", "Warriors", False), 1))
